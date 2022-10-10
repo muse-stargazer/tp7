49c1fc272838fa18b5bcb38497213d934bcf198d
muse_stargazer <bertadams00@gmail.com>
2022-10-10 04:11:24 +0000

    added nav bar, kept JS I wanted and deleted other window.onload event, proceeding with lecture

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// JS for TP5
+// JS for TP7
 
 
 //funtion to load a file from the url "fromFile" into the object identified as "whereTo"
@@ -26,52 +26,50 @@ function loadFileInto(fromFile, whereTo) {
 
 }
 
+//New Recipe Object
+function Recipe (recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  this.recipeName
+  this.contributor
+  this.imageURL
+  this.ingredients
+  this.equipment
+  this.directions
+}
 
 window.onload = function() {
 
-  loadFileInto("ingredients.html", "#ingredients ul");
-
-  loadFileInto("equipment.html", "#equipment ul");
-
-  loadFileInto("directions.html", "#directions ol");
-
   document.querySelector("#header h1").classList.add("tp");
-  
-  //trying to get header h1 responsive through js since media queries are no longer effective on it. JS media queries are eluding me. 
-  //document.querySelector("header h1")
   
   document.querySelector("#header h1.tp").onclick = function() {
     this.classList.toggle("white");
   }
 
-  document.querySelector("#bigflex #ingredients").onclick = function() {
-    this.classList.toggle("tpshow");
-  }
-
-  document.querySelector("#bigflex #equipment").onclick = function() {
-    this.classList.toggle("tpshow");
-  }
-
-  document.querySelector("#bigflex #directions").onclick = function() {
-    this.classList.toggle("tpshow");
-  }
-
-  //document.querySelector("#thanks background-image").classList.add("transparent");
-
-  //document.querySelector("thanks background-image.transparent").style.opacity = "0.5";
-
   document.querySelector("#thanks h1").classList.add("bg");
 
   document.querySelector("#footer").classList.add("bg");
 
-  setTimeout(function() {
-    document.querySelector("#bigflex #ingredients ul").innerHTML += '<li>A smattering of <span class="notice">sunshine dust</span></li>';
-
-    document.querySelector("#bigflex #equipment ul").innerHTML += '<li><span class="notice"><span class="measure">36-inch</span> claw of a dragon</span> <span class="measure">(Species up to personal preference)</span></li>';
-
-    document.querySelector("#bigflex #directions ol").innerHTML += '<li>Use <span class="notice">dragon claw</span> to shave <span class="notice">sunshine dust</span> <span class="measure">sparingly</span> over flan.</li><li>Enjoy!</li>';
-  }, 100);
-
 }; // end window.onload
 
+
+
+ //deleted window.onload functions from tp6:
+/*window.onload = function() {
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol"); 
+  /*document.querySelector("#bigflex #ingredients").onclick = function() {
+    this.classList.toggle("tpshow");
+  }
+  document.querySelector("#bigflex #equipment").onclick = function() {
+    this.classList.toggle("tpshow");
+  }
+  document.querySelector("#bigflex #directions").onclick = function() {
+    this.classList.toggle("tpshow");
+  }
+   /*setTimeout(function() {
+    document.querySelector("#bigflex #ingredients ul").innerHTML += '<li>A smattering of <span class="notice">sunshine dust</span></li>';
+    document.querySelector("#bigflex #equipment ul").innerHTML += '<li><span class="notice"><span class="measure">36-inch</span> claw of a dragon</span> <span class="measure">(Species up to personal preference)</span></li>';
+    document.querySelector("#bigflex #directions ol").innerHTML += '<li>Use <span class="notice">dragon claw</span> to shave <span class="notice">sunshine dust</span> <span class="measure">sparingly</span> over flan.</li><li>Enjoy!</li>';
+  }, 100);
+}*/
 
